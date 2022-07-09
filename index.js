db8a4efaeab07f51ebdc11e778ae7e01c720854c
Alexis <alexis@Kali.Andrew>
2022-07-09 15:16:43 -0300

    Cleaner code

--- index.js
+++ index.js
@@ -9,37 +9,39 @@ const json = require('./SkySeasons.json');
 
 
 ///////////////////////////// FUNCTIONS /////////////////////////////
-function seasonOptions(message, jsonMap, rol){
+function seasonOptions(message, jsonMap, HexColor){
     
     const embed = new MessageEmbed().setTitle("Temporadas de SKY")
     .setAuthor(message.author.username, message.author.displayAvatarURL())
-    .setColor(rol.hexColor);
+    .setColor(HexColor);
 
     jsonMap.forEach(
         (sky, val) => embed.addField(val, Object.entries(sky).reduce(
             (prev, curr) =>{
-                return curr[0] + " " + curr[1].precio+ "\n" + prev;
+                return prev + curr[0] + " " + curr[1].precio + "\n";
             }, "")
         )
     );
 
     message.channel.send({ embeds: [embed] });
 }
-function accFiles(sky, message, accMap, rol){
+function accFiles(sky, message, acc, HexColor){
 
     const embed = new MessageEmbed().setTitle(sky[0] + "-" + sky[1] + ": " + sky[2])
-    .setColor(rol.hexColor)
-    .addField("precio", accMap.get(sky[2]).precio)
-    .setImage('attachment://' + accMap.get(sky[2]).file);;
+    .setColor(HexColor)
+    .addField("precio", acc.precio)
+    .setImage('attachment://' + acc.file);;
 
-    message.channel.send({ embeds: [embed], files: ['./' + accMap.get(sky[2]).file] });
+    const msg = { embeds: [embed], files: ['./' + acc.file] };
+
+    message.channel.send(msg);
 }
 
-function travelsAccess(sky, message, travelMap, rol){
+function travelsAccess(sky, message, access, HexColor){
 
-    const accMap = new Map(Object.entries(travelMap.get(sky[1])));
+    const accMap = new Map(Object.entries(access));
     const embed = new MessageEmbed().setTitle(sky[0] + ": " + sky[1])
-    .setColor(rol.hexColor);
+    .setColor(HexColor);
 
     if(sky.length <= 2 || !accMap.has(sky[2])){
         accMap.forEach(
@@ -51,30 +53,41 @@ function travelsAccess(sky, message, travelMap, rol){
     
         message.channel.send({ embeds: [embed] });
     }else{
-        accFiles(sky,message,accMap, rol);
+        accFiles(sky, message, accMap.get(sky[2]), HexColor);
     }
 
 }
 
-function seasontravels(sky, message, jsonMap, rol){
-    
+function seasontravels(sky, message, jsonMap, HexColor){
+
     const travelMap = new Map(Object.entries(jsonMap.get(sky[0])));
     const embed = new MessageEmbed().setTitle(sky[0])
-    .setColor(rol.hexColor);
+    .setColor(HexColor);
 
     if (sky.length <= 1 || !travelMap.has(sky[1])){
         travelMap.forEach(
             (values, viajero) => embed.addField(viajero, Object.entries(values).reduce(
-                (curr, prev) =>{
-                    return prev[0] + ": 0 :candle:, 0 :heart:" + "\n" + curr;
+                (prev, curr) =>{
+                    const p = (curr[0] !== "precioTotal")? curr[1].precio : curr[1];               
+                    return prev + curr[0] + ": " + p + "\n";
                 }, "")
             )
         );
 
         message.channel.send({ embeds: [embed] });
     }else{
-        travelsAccess(sky, message, travelMap, rol);
+        travelsAccess(sky, message, travelMap.get(sky[1]), HexColor);
     }   
+}
+
+function rolColor(message){
+    const roles = message.guild.roles.cache;
+    
+    const rol = roles.find(r => (roles.get(r.id).members.filter(
+        (k) => k.user.username === message.author.username 
+    ).size !== 0) && r.name.includes("Cape"));
+    
+    return rol.hexColor;
 }
 
 ///////////////////////////// BOT REAL /////////////////////////////
@@ -87,12 +100,10 @@ client.on("message", message => {
         const jsonMap = new Map(Object.entries(json));
         const msg = message.content.split(" ");
         const sSon = ((msg.length <= 1)? "" : msg[1]).split("/");
-        const rol = message.guild.roles.cache.find(r => (message.guild.roles.cache.get(r.id).members.filter(
-            (k) => k.user.username === message.author.username 
-        ).size !== 0) && r.name.includes("Cape"));
+        const HexColor = rolColor(message);
         
         if (msg[0] === PREFIX){
-            (!jsonMap.has(sSon[0]))? seasonOptions(message, jsonMap, rol) : seasontravels(sSon, message, jsonMap, rol);
+            (!jsonMap.has(sSon[0]))? seasonOptions(message, jsonMap, HexColor) : seasontravels(sSon, message, jsonMap, HexColor);
         }
     }
 });
